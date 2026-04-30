5a5154b2427b82570a93f96bb498120be3bf8b9f
rayStettler <rps210001@utdallas.edu>
2026-04-30 12:49:29 +0000

    it's something???

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,51 @@
-// main.js
+// main js
 
-console.log("Hello, World!");
+let grid = document.querySelectorAll(".grid-container"); 
+let divs = document.querySelectorAll(".grid-container div"); 
+//let divs = document.querySelection(".grid-container div"); 
+
+let rotation = 0;
+document.addEventListener("click", function () {
+    // let min = 10; 
+    // let max = 40; 
+    rotation += randomNumber(1, 200); 
+    
+    document.body.style.transform = `rotate(${rotation}deg)`; 
+}) 
+
+//let elements = document.querySelectorAll("body *"); 
+//console.log(elements.length); 
+//let divs = document.querySelection(".grid-container div"); 
+ 
+
+function randomize() {
+    
+    divs.forEach(function(div) {
+        let scale = randomNumber(.5, 1.2);
+        let translateX = randomNumber(0, 50);
+        let translateY = randomNumber(0, 360);
+        let rotate = randomNumber(0, 360);
+
+        if(Math.random() > .5) {
+            scale = 1; 
+            translateX = randomNumber(0, 80);
+            translateY = randomNumber(0, 80); 
+        }
+
+        div.style.transform = 
+            `scale(${scale})
+            translate(${translateX}%, ${translateY}%)
+            rotate(${rotate}deg)`; 
+
+        
+    }); 
+
+    console.log(divs.length, "randomize! "); 
+ 
+} 
+
+document.addEventListener("click", randomize);
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + 1) + min; 
+}
