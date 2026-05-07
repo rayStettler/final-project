c9d58898aa6530f072189e6224d0e106a7790b87
rayStettler <rps210001@utdallas.edu>
2026-05-07 20:01:52 +0000

    delete useless code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,58 +1,4 @@
 // main js
-
-let grid = document.querySelectorAll(".grid-container"); 
-let divs = document.querySelectorAll(".grid-container div"); 
-//let divs = document.querySelection(".grid-container div"); 
-
-const myButton = document.getElementById('myButton'); // Select your button
-
-let rotation = 0;
-/*
-document.addEventListener("click", function () {
-    // let min = 10; 
-    // let max = 40; 
-    rotation += randomNumber(1, 200); 
-    
-    document.body.style.transform = `rotate(${rotation}deg)`; 
-}) 
-*/ 
-//let elements = document.querySelectorAll("body *"); 
-//console.log(elements.length); 
-//let divs = document.querySelection(".grid-container div"); 
- 
-
-function randomize() {
-    
-    divs.forEach(function(div) {
-        let scale = randomNumber(.5, 1.2);
-        let translateX = randomNumber(0, 50);
-        let translateY = randomNumber(0, 360);
-        let rotate = randomNumber(0, 360);
-
-        if(Math.random() > .5) {
-            scale = 1; 
-            translateX = randomNumber(0, 80);
-            translateY = randomNumber(0, 80); 
-        }
-
-        div.style.transform = 
-            `scale(${scale})
-            translate(${translateX}%, ${translateY}%)
-            rotate(${rotate}deg)`; 
-
-        
-    }); 
-
-    console.log(divs.length, "randomize! "); 
- 
-} 
-document.addEventListener("click", randomize);
-
-function randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min) + 1) + min; 
-}
-// When the user clicks on <div>, open the popup
-
 // Initialize a global counter
 let evilCount = 0;
 let normCount = 0; 
@@ -60,7 +6,6 @@ let totalCount = 0;
 let freakOutCount = 0; 
 
 const messageElement = document.getElementById('message');
-
 
 // Evil buttons
 document.querySelectorAll('.evil-btn').forEach(button => {
@@ -101,7 +46,6 @@ document.querySelectorAll('.freakOutPage').forEach(button=>{
     });
     
 });
-
 
 function messages(isEvil)
 {
